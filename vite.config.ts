201fc1bfaedf2bce4f142b30e215f9ff796102d7
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-07-20 00:54:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/my-todo-app/' // Viktigt! Snedstreck i början och slut
+  base: '/my-todo-app/', // Viktigt! Snedstreck i början och slut
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 });
